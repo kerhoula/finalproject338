9ce686dda95ab768f600ff794d5de8186a288008
Anna Kerhoulas <annakerhoulas@Annas-Laptop-194.local>
2026-05-20 23:32:21 +0100

    lots of progress tonight! may 20 git push !

---
+++ guinness.js
@@ -0,0 +1,51 @@
+const button = document.getElementById("split-button");
+const foam = document.getElementById("foam");
+const message = document.getElementById("split-message");
+
+let lastHeight = null;
+
+button.addEventListener("click", () => {
+
+    const possibleHeights = [
+        120,
+        140,
+        168,
+        195,
+        215
+    ];
+
+    let newHeight;
+
+    // KEEP ROLLING UNTIL DIFFERENT
+    do{
+        newHeight =
+            possibleHeights[
+                Math.floor(Math.random() * possibleHeights.length)
+            ];
+
+    } while(newHeight === lastHeight);
+
+    lastHeight = newHeight;
+
+    foam.style.height = newHeight + "px";
+
+    // SUCCESS
+    if(newHeight === 168){
+
+        message.textContent =
+            "Perfect split. The locals approve.";
+
+    } else if(newHeight < 168){
+
+        message.textContent =
+            "Too high. The G remains unsplit.";
+
+    } else {
+
+        message.textContent =
+            "Too low. Mix in a water eh?";
+    }
+
+});
+
+// This is super duper chatgpt
